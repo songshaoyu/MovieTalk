a795438c60280f9f6b307039d9e17906f409e940
songshaoyu <ahu8756@gmail.com>
2017-04-02 23:58:23 +0800

    implement movie's crud % view

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,4 +2,48 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
   end
+
+  def show
+    @movie = Movie.find(params[:id])
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      redirect_to movies_path
+    else
+      render :new
+    end
+  end
+
+  def destrpy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    redirect_to movies_path
+  end
+
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+    if @movie.update(movie_params)
+      flash[:notice] = "update success"
+      redirect_to movies_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :description)
+  end
+
 end
