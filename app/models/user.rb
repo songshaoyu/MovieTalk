11da166da62542610c4aba9fde55a9879ff5d8aa
songshaoyu <ahu8756@gmail.com>
2017-04-03 21:00:10 +0800

    implement is_member_of?(movie)

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,8 @@ class User < ApplicationRecord
   has_many :movie_relationships
   has_many :favourite_movies, :through => :movie_relationships,
   :source => :movie
+
+  def is_member_of?(movie)
+    favourite_movies.include?(movie)
+  end
 end
