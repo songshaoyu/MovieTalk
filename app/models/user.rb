5ab833c587b6663bb3c01c9449591859d34e1ca5
songshaoyu <ahu8756@gmail.com>
2017-04-03 20:42:47 +0800

    implement movie relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   has_many :movies
   has_many :reviews
+
+  has_many :movie_relationships
+  has_many :favourite_movies, :through => :movie_relationships,
+  :source => :movie
 end
