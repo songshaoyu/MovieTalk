07e754ff4da5ba0de16a3f0a06fe06a2b1a0a5af
songshaoyu <ahu8756@gmail.com>
2017-04-03 17:26:44 +0800

    implement reviews pagination with will_paginate

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,7 @@ class MoviesController < ApplicationController
 
   def show
     @movie = Movie.find(params[:id])
-    @reviews = @movie.reviews.recent
+    @reviews = @movie.reviews.recent.paginate(:page => params[:page], :per_page => 6)
   end
 
   def new
